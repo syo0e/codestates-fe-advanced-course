771ddd751983d0dabce4bd00edf18e78460eec11
syo0e <kimdlzp@gmail.com>
2022-08-05 23:49:03 +0900

    Updates

---
+++ precache-manifest.8ab2280d4606b6816c53eaa458e9a613.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "78380d9254fa26901e72",
+    "url": "/codestates-fe-advanced-course/static/js/main.78380d92.chunk.js"
+  },
+  {
+    "revision": "3fa58f8e526e7176367b",
+    "url": "/codestates-fe-advanced-course/static/js/1.3fa58f8e.chunk.js"
+  },
+  {
+    "revision": "d11486ef176154e3e4ad",
+    "url": "/codestates-fe-advanced-course/static/js/runtime~main.d11486ef.js"
+  },
+  {
+    "revision": "72783dccff7ce30cc3aeb71713f23b95",
+    "url": "/codestates-fe-advanced-course/index.html"
+  }
+];
